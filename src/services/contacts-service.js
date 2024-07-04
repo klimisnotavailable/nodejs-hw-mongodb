f2ed0e7afac02974b801f1434600450cdde1ad17
klimisnotavailable <klimgrigor28@gmail.com>
2024-07-04 13:18:47 +0300

    правки

--- src/services/contacts-service.js
+++ src/services/contacts-service.js
@@ -8,19 +8,21 @@ export const postContact = data => Contact.create(data);
 
 export const deleteContact = id => Contact.findByIdAndDelete(id);
 
-export const upsetContact = async (filter,payload,options={}) => {
-    const result = await Contact.findOneAndUpdate(filter,payload,{
+export const upsetContact =  (filter,payload,options={}) => {
+
+    const result = Contact.findOneAndUpdate(filter,payload,{
     new:true,
     ...options});
+    console.log("before operation");
 
-if (!result) return null;
+
+    if (!result) return null;
 
 // const isNew = data && data.lastErrorObject && data.lastErrorObject.upserted;
 
-return {
-    data: result,
-};
-
+    return {
+        data: result,
+    };
 };
 
 // export const upsetContact = async (filter,payload,options={}) => Contact.findOneAndUpdate(filter,payload,{new:true,...options});
