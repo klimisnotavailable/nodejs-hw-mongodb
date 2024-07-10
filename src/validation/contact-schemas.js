c22210cb0dc6c2995cfc3658a2cb630400c08d0d
klimisnotavailable <klimgrigor28@gmail.com>
2024-07-10 13:03:54 +0300

    Update contact-schemas.js

--- src/validation/contact-schemas.js
+++ src/validation/contact-schemas.js
@@ -2,8 +2,8 @@ import Joi from "joi";
 
 export const contactAddSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
-    phoneNumber: Joi.string().min(3).max(20),
-    email: Joi.string().min(3).max(20).required(),
+    phoneNumber: Joi.string().min(3).max(20).required(),
+    email: Joi.string().min(3).max(20),
     isFavourite: Joi.boolean().default(false),
     contactType: Joi.string().allow("personal","work"),
 });
