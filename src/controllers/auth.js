9f9ba061f14234423fcfd3d9c2fdae7f755b7b28
klimisnotavailable <klimgrigor28@gmail.com>
2024-07-18 09:30:35 +0300

    hw

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -11,7 +11,7 @@ export const registerUserController = async (req, res) => {
 
   const user = await registerUser(req.body);
 
-  res.json({
+  res.status(201).json({
       status: 201,
       message: 'Successfully registered a user!',
       data: user,
